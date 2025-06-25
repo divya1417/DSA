cc8902be949027e127a95717865555173f34f8a3
Divya Pant <154441429+divya1417@users.noreply.github.com>
2025-06-25 21:06:11 +0530

    Time: 1 ms (99.71%) | Memory: 42.4 MB (6.62%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -1,12 +1,15 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-      char r =0;
-      for(char e : s.toCharArray()){
-        r=(char)(r^e);
-      } 
-      for(char e : t.toCharArray()){
-        r =(char)(r^e);
-      } 
-      return r;
+        char result = 0;
+
+        for (char c : s.toCharArray()) {
+            result ^= c;
+        }
+
+        for (char c : t.toCharArray()) {
+            result ^= c;
+        }
+
+        return result; // The extra character
     }
 }
